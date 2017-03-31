9edcac486f0434ae5b76bd549c553b6f5b7dc18c
LeslieKornes <Lesliekornes@gmail.com>
2017-03-30 21:37:42 -0400

    first commit

---
+++ Customer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace CustomerClassPractice
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)     //this inherits the parameterless ctors list instantiation
+           : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)    //this inherits from the (id) ctor, thereby inheriting the instantiated list as well
+            :this(id)                             
+        {
+            this.Name = name;
+        }
+                                                //NOT GOOD CODING PRACTICE THOUGH
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomerClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(543, "Nino");
+
+            //this is an object initializer-quickly initializes an object w/o the need to call one of its ctors
+            var customer2 = new Customer        
+            {
+                Name = "Andrew",
+                Id = 1007
+            };
+        }
+    }
+}
